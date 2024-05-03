60054e02880e9654c756f465e80b8ede6f1026e3
Nik <nik.stebih@student.um.si>
2024-05-03 14:39:07 +0200

    added parent property to interface classes.

--- Interface/Encryption.py
+++ Interface/Encryption.py
@@ -4,18 +4,23 @@ import tkinter as tk
 class Encryption(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
         tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
 
         self.decryption_label = tk.Label(self, text="Cipher", font=("Helvetica", 16))
         self.decryption_label.pack()
 
         self.encrypt_button = tk.Button(self, height=2, width=20, text="Encrypt",
-                                        command=lambda: parent.cipher.encrypt(parent.file, parent.selected_mode.get(),
-                                                                              parent.file_format, self.update_speed))
+                                        command=lambda: self.parent.cipher.encrypt(parent.file,
+                                                                                   parent.selected_mode.get(),
+                                                                                   parent.file_format,
+                                                                                   self.update_speed))
         self.encrypt_button.pack(pady=10)
 
         self.decrypt_button = tk.Button(self, height=2, width=20, text="Decrypt",
-                                        command=lambda: parent.cipher.decrypt(parent.file, parent.selected_mode.get(),
-                                                                              parent.file_format, self.update_speed))
+                                        command=lambda: self.parent.cipher.decrypt(parent.file,
+                                                                                   parent.selected_mode.get(),
+                                                                                   parent.file_format,
+                                                                                   self.update_speed))
         self.decrypt_button.pack(pady=10)
 
         self.speed_label = tk.Label(self, text="Speed", font=("Helvetica", 16))
